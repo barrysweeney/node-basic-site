5a2539efc94409bccce6b0f77a6e075b2ff953b6
barrysweeney <barrysweeney172@live.co.uk>
2020-05-07 13:28:06 +0100

    add description for how to navigate

--- index.js
+++ index.js
@@ -10,21 +10,22 @@ http
 
     if (filename === "./") {
       filename = "./index.html";
+    } else {
+      filename = filename.concat(".html");
     }
 
     fs.readFile(filename, function (err, data) {
       if (err) {
         fs.readFile(errorFile, function (err, errorData) {
-          if (err) throw err;
           res.writeHead(404, { "Content-Type": "text/html" });
           res.write(errorData);
           return res.end();
         });
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        return res.end();
+        return;
       }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
     });
   })
   .listen(8080);
